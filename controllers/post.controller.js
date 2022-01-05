54fa202af5364a1047abb0c261734fc4d26ca105
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2022-01-05 15:22:33 +0530

    Pagination frontend

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -30,14 +30,14 @@ const getHomePage = asyncHandler(async (req, res, next) => {
 	// pagination
 	const pagination = {};
 
-	// pagination
-	const page = parseInt(req.query.page, 10) || 1;
-
 	// number of objects per page
 	const limit = parseInt(req.query.limit, 10) || 5;
+	const total = await PostModel.countDocuments();
+	const max = Math.ceil(total/limit);
+	const page = Math.min(parseInt(req.query.page, 10) || 1, max);
+
 	const startIndex = (page - 1) * limit;
 	const endIndex = page * limit;
-	const total = await PostModel.countDocuments();
 
 	// skipping the objects which is in previous pages, and limiting to limit
 	query = query.skip(startIndex).limit(limit);
@@ -62,11 +62,15 @@ const getHomePage = asyncHandler(async (req, res, next) => {
 			title: "No posts yet",
 			subtitle: "Why don't you start the party :uganda:",
 			body: "didn't you read the title and subtitle :mhm:",
+			author: {
+				id: 0,
+				name: ""
+			},
 			pagination: pagination,
 		};
 	}
 
-	res.render("home", { posts });
+	res.render("home", { page, posts, pagination, limit, total, max });
 });
 
 const postCreatePage = asyncHandler(async (req, res, next) => {
--- public/js/home.js
+++ public/js/home.js
@@ -1,122 +1,126 @@
 var mobile = window.matchMedia("screen and (max-width: 1069px)");
-var currPage = 1;
 
 const dateFormat = new Intl.DateTimeFormat([], {
-	year: "numeric",
-	month: "numeric",
-	day: "numeric",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric"
 });
 const timeFormat = new Intl.DateTimeFormat([], {
-	hour: "numeric",
-	minute: "numeric",
+    hour: "numeric",
+    minute: "numeric"
 });
 let firstNews = document.getElementsByClassName("news-item-view-button")[0];
 if (firstNews) {
-	firstNews.click();
-	toggleCollapseNewsItem();
+    firstNews.click();
+    if (mobile.matches) toggleCollapseNewsItem();
 }
 
 setTimeout(displayLocalTimes);
 
 function displayLocalTimes() {
-	let today = new Date();
-	let yesterday = new Date(today.getDate() - 1);
-	let displayDate;
-	let ItemHeaders = document.getElementsByClassName("news-item-header");
-	for (let i = 0; i < ItemHeaders.length; i++) {
-		let date = new Date(ItemHeaders[i].dataset.dateCreated);
-		if (isNaN(date.getTime())) {
-			continue;
-		}
-		if (today.toDateString() === date.toDateString()) {
-			displayDate = "Today";
-		} else if (yesterday.toDateString() === date.toDateString()) {
-			displayDate = "Yesterday";
-		} else {
-			displayDate = dateFormat.format(date);
-		}
-		ItemHeaders[i].innerHTML = displayDate + " | " + timeFormat.format(date);
-	}
+    let today = new Date();
+    let yesterday = new Date(today.getDate() - 1);
+    let displayDate;
+    let ItemHeaders = document.getElementsByClassName("news-item-header");
+    for (let i = 0; i < ItemHeaders.length; i++) {
+        let date = new Date(ItemHeaders[i].dataset.dateCreated);
+        if (isNaN(date.getTime())) {
+            continue;
+        }
+        if (today.toDateString() === date.toDateString()) {
+            displayDate = "Today";
+        } else if (yesterday.toDateString() === date.toDateString()) {
+            displayDate = "Yesterday";
+        } else {
+            displayDate = dateFormat.format(date);
+        }
+        ItemHeaders[i].innerHTML = displayDate + " | " + timeFormat.format(date);
+    }
 }
 
 function toggleCollapseNewsItem() {
-	let items =
-		document.getElementsByClassName("news-items-container")[0] || false;
-	let news_container =
-		document.getElementsByClassName("news-content-container")[0] || false;
-	if (items.classList.contains("collapsed")) {
-		items.classList.remove("collapsed");
-		news_container.classList.remove("items-collapsed");
-	} else {
-		items.classList.add("collapsed");
-		news_container.classList.add("items-collapsed");
-	}
-	if (!mobile.matches) {
-		let news = document.getElementsByClassName("news-content")[0];
-		news.style.opacity = "0";
-		setTimeout(() => {
-			news.style.transition = "opacity .4s ease";
-			news.style.opacity = "1";
-			setTimeout(() => (news.style.transition = "none"), 400);
-		}, 300);
-	}
+    let items =
+        document.getElementsByClassName("news-items-container")[0] || false;
+    let news_container =
+        document.getElementsByClassName("news-content-container")[0] || false;
+    if (items.classList.contains("collapsed")) {
+        items.classList.remove("collapsed");
+        news_container.classList.remove("items-collapsed");
+    } else {
+        items.classList.add("collapsed");
+        news_container.classList.add("items-collapsed");
+    }
+    if (!mobile.matches) {
+        let news = document.getElementsByClassName("news-content")[0];
+        news.style.opacity = "0";
+        setTimeout(() => {
+            news.style.transition = "opacity .4s ease";
+            news.style.opacity = "1";
+            setTimeout(() => (news.style.transition = "none"), 400);
+        }, 300);
+    }
 }
 
 function newsItemClicked(
-	title,
-	authorName,
-	authorId,
-	body,
-	updatedAt,
-	currUserId,
-	postId
+    title,
+    authorName,
+    authorId,
+    body,
+    updatedAt,
+    currUserId,
+    postId
 ) {
-	let newsContent = document.getElementsByClassName("news-content")[0];
-	let newsContentMeta = document.getElementsByClassName(
-		"news-content-metadata"
-	)[0];
-	let newsContentDelete = document.getElementsByClassName(
-		"news-content-delete"
-	)[0];
-	newsContent.innerText = body;
-	newsContentMeta.innerHTML = `${title} - ${authorName} | ${updatedAt}`;
-	if (authorId === currUserId) {
-		newsContentDelete.innerHTML = "DELETE";
-		newsContentDelete.dataset.id = postId;
-	} else newsContentDelete.innerHTML = "";
-	if (mobile.matches) {
-		toggleCollapseNewsItem();
-	}
+    let newsContent = document.getElementsByClassName("news-content")[0];
+    let newsContentMeta = document.getElementsByClassName(
+        "news-content-metadata"
+    )[0];
+    let newsContentDelete = document.getElementsByClassName(
+        "news-content-delete"
+    )[0];
+    newsContent.innerText = body;
+    newsContentMeta.innerHTML = `${title} - ${authorName} | ${updatedAt}`;
+    if (authorId === currUserId) {
+        newsContentDelete.innerHTML = "DELETE";
+        newsContentDelete.dataset.id = postId;
+    } else newsContentDelete.innerHTML = "";
+    if (mobile.matches) {
+        toggleCollapseNewsItem();
+    }
 }
 
 function gotPageInput() {
-	let pageInput = document.getElementById("pagination-page-input");
-	let val = +pageInput.value;
-	if (Number.isInteger(val) && 1 <= val && val <= 20) {
-		currPage = val;
-		pageInput.blur();
-		return true;
-	}
-	return false;
-}
-
-function updatePageInputValue() {
-	document.getElementById("pagination-page-input").value = `${currPage}`;
+    let pageInput = document.getElementById("pagination-page-input");
+    let val = +pageInput.value;
+    if (page === val) return pageInput.blur();
+    if (Number.isInteger(val) && 1 <= val && val <= maxPages) {
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('page', val.toString());
+        window.location.search = urlParams;
+    } else {
+        pageInput.value = page.toString();
+        pageInput.blur();
+    }
 }
 
 function pageNext() {
-	currPage = Math.min(20, currPage + 1);
-	updatePageInputValue();
+    if (pagination.next) {
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('page', pagination.next.page);
+        window.location.search = urlParams;
+    }
 }
 
 function pagePrev() {
-	currPage = Math.max(1, currPage - 1);
-	updatePageInputValue();
+    if (pagination.prev) {
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('page', pagination.prev.page);
+        window.location.search = urlParams;
+    }
 }
 
 async function deletePost(id) {
-	const res = await fetch(`/posts/${id}`, {
-		method: "DELETE",
-	}).then((data) => data.json());
-	console.log(res);
+    const res = await fetch(`/posts/${id}`, {
+        method: "DELETE"
+    }).then((data) => data.json());
+    console.log(res);
 }
