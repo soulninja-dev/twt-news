c0f6276ed5616441fb150a96a75ac1f7afa14393
SoulNinja-dev <aadhi123awesome@gmail.com>
2022-03-10 15:01:41 +0530

    process.env

--- controllers/oauth.controller.js
+++ controllers/oauth.controller.js
@@ -5,8 +5,7 @@ const asyncHandler = require("../middleware/async");
 const getJWTAccessToken = require("../utils/getAccessToken");
 
 // constants
-const redirect_url =
-	"https://discord.com/api/oauth2/authorize?client_id=930166100904255549&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&response_type=code&scope=identify";
+const redirect_url = process.env.REDIRECT_URL;
 const userinfo_url = "https://discord.com/api/users/@me";
 
 const expireTime = 50 * 24 * 60 * 60;
